996c5d99f2688318c6276c3b4a761dcc3b9c57cf
Emanuel Facundo <emanuel.facundo14@gmail.com>
2024-05-06 21:41:37 -0300

    feat: adiciona controller personalizavel para realizar login via devise

---
+++ app/controllers/sellers/sessions_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Sellers
+  class SessionsController < Devise::SessionsController
+    before_action :redirect_when_already_signed_in, only: %i[new create]
+
+    # GET /sellers/sign_in
+    # def new
+    #   super
+    # end
+
+    # POST /sellers/sign_in
+    def create
+      super
+      Current.seller = resource if seller_signed_in?
+    end
+
+    # DELETE /sellers/sign_out
+    # def destroy
+    #   super
+    # end
+
+    private
+    def redirect_when_already_signed_in
+      redirect_to request.referrer, alert: t("sellers.already_signed_in") if seller_signed_in?
+    end
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :sellers, controllers: {
-    registrations: "sellers/registrations"
+    registrations: "sellers/registrations",
+    sessions: "sellers/sessions"
   }
 
   root to: "home#index"
