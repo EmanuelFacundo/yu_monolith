9b909cf726d5570a34425249fa9c359d9d9303a4
Emanuel Facundo <emanuel.facundo14@gmail.com>
2024-05-08 23:33:22 -0300

    refactor: adiciona em qual versao da input mask ela foi adicionada ao projeto

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,4 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 
-pin 'inputmask'
+pin 'inputmask' # 5.0.8
