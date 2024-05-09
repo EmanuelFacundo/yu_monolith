79f1a9d91943ad2798ba2221c511623e7e154c40
Emanuel Facundo <38675971+EmanuelFacundo@users.noreply.github.com>
2024-05-08 21:14:23 -0300

    feat: Adiciona validates para os models Seller e Buyer e User (#13)
    * feat: Adiciona validates para os models Seller e Buyer
    
    * feat: adiciona validates para o numero de telefone do usuario

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class Buyer < ApplicationRecord
   include Userable
 
-  validates :document, presence: true
+  validates :document, presence: true, uniqueness: true
+  delegate :first_name, :last_name, :phone_number, to: :user, allow_nil: true
 end
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -5,9 +5,9 @@ class Seller < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :email, presence: true, uniqueness: true
-  validates :document, presence: true, uniqueness: true, format: { with: REGEXP::CPF_OR_CNPJ }
-  validates :password, presence: true, length: { minimum: 8 }, format: { with: REGEXP::EMAIL }
+  validates :email, presence: true, uniqueness: true, format: { with: REGEXP::EMAIL }
+  validates :document, presence: true, uniqueness: true, format: { with: REGEXP::BRAZIL::CPF_OR_CNPJ }
+  validates :password, presence: true, length: { minimum: 8 }, format: { with: REGEXP::PASSWORD }
   delegate :first_name, :last_name, :phone_number, to: :user, allow_nil: true
 
   def full_name
