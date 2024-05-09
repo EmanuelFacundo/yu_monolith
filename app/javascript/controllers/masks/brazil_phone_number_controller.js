777b41d54041281a2b7c5c033d623451646ab8c7
Emanuel Facundo <emanuel.facundo14@gmail.com>
2024-05-08 23:31:41 -0300

    refactor: Define espaco correto para os sprinkles de mascaras

--- app/javascript/controllers/masks/brazil_phone_number_controller.js
+++ app/javascript/controllers/masks/brazil_phone_number_controller.js
@@ -4,7 +4,7 @@ import "inputmask";
 export default class extends Controller {
   connect() {
     new Inputmask({
-      regex: "\\([1-9]{2}\\) [0-9]{4,5}-[0-9]{4}",
+      regex: "\\([1-9]{2}\\) [0-9]{4}-[0-9]{4}|\\([1-9]{2}\\) [0-9]{5}-[0-9]{4}",
       keepStatic: true,
     }).mask(this.element);
   }
