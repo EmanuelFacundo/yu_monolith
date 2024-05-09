6f737acac57164e7d4161fb414b12f87585ceb38
Emanuel Facundo <emanuel.facundo14@gmail.com>
2024-05-08 21:10:33 -0300

    feat: adiciona validates para o numero de telefone do usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   delegated_type :userable, types: %w[Buyer Seller], dependent: :destroy
 
   validates :first_name, :last_name, :phone_number, presence: true
+  validates :phone_number, format: { with: REGEXP::BRAZIL::PHONE_NUMBER }
 end
