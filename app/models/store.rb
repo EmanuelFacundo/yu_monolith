219106f85fa9871d2ac9fa2aa9a1b37b505dd123
Emanuel Facundo <emanuel.facundo14@gmail.com>
2024-05-20 21:57:03 -0300

    feat: adiciona validacao regex para atributos de Store

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,6 +2,8 @@
 
 class Store < ApplicationRecord
   validates :status, presence: true
+  validates :phone_number, format: { with: REGEXP::BRAZIL::PHONE_NUMBER }, allow_blank: true
+  validates :email, format: { with: REGEXP::EMAIL }, allow_blank: true
 
   belongs_to :seller, foreign_key: :user_id, inverse_of: :store
 end
