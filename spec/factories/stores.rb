e1698104f24aca5eebcb7c32301f32ec1e490ba7
Emanuel Facundo <emanuel.facundo14@gmail.com>
2024-05-20 21:54:49 -0300

    feat: adiciona factory para o model Store

---
+++ spec/factories/stores.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :store do
+    name { 'my store' }
+    email { 'teste@store.com' }
+    phone_number { '(11) 94002-8922' }
+
+    trait :inactive do
+      status { 'inactive' }
+    end
+
+    trait :active do
+      status { 'active' }
+    end
+
+    trait :with_seller do
+      seller { create(:seller) }
+    end
+
+    factory :store_with_seller, traits: %i[with_seller]
+    factory :inactive_store, traits: %i[inactive]
+    factory :active_store, traits: %i[active]
+  end
+end
