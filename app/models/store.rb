a0492581551c3b0f83df61f3eb39842bcf256181
Emanuel Facundo <emanuel.facundo14@gmail.com>
2024-05-20 22:07:21 -0300

    feat: adiciona validacao para inclusao do status na lista

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Store < ApplicationRecord
-  validates :status, presence: true
+  validates :status, presence: true, inclusion: { in: STORE::STATUS }
   validates :phone_number, format: { with: REGEXP::BRAZIL::PHONE_NUMBER }, allow_blank: true
   validates :email, format: { with: REGEXP::EMAIL }, allow_blank: true
 
