86bfc8031c9bd71502525a83b86f3dd4d19b9e5f
Emanuel Facundo <emanuel.facundo14@gmail.com>
2024-06-01 15:22:40 -0300

    feat: adding function to generate favicon derivatives when save Store

--- app/models/store.rb
+++ app/models/store.rb
@@ -9,5 +9,15 @@ class Store < ApplicationRecord
   validates :phone_number, format: { with: REGEXP::BRAZIL::PHONE_NUMBER }, allow_blank: true
   validates :email, format: { with: REGEXP::EMAIL }, allow_blank: true
 
+  before_save :generate_favorite_icon_derivatives
+
   belongs_to :seller, foreign_key: :user_id, inverse_of: :store
+
+  private
+
+  def generate_favorite_icon_derivatives
+    return unless favicon_attacher.file
+
+    favicon_derivatives!
+  end
 end
